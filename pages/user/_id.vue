c0727608e326ba2152d33c293d38121472bcdfaf
Shigehiro IDANI <idani@hirotae.com>
2020-04-12 12:44:23 +0900

    ユーザ詳細画面を追加

---
+++ pages/user/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>/user/{{ userId }}</div>
+</template>
+
+<script>
+export default {
+  name: 'UserDetail',
+  validate({ params }) {
+    // 数値でなければならない
+    return /^\d+$/.test(params.id)
+  },
+  computed: {
+    userId() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style scoped></style>
