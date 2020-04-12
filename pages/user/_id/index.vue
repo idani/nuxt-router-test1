da83a97138c0fc09392cb153c4c42551b2c5fb27
Shigehiro IDANI <idani@hirotae.com>
2020-04-12 14:26:18 +0900

    アンダースコアプレフィックスフォルダ

--- pages/user/_id/index.vue
+++ pages/user/_id/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>/user/{{ userId }}</div>
+  <v-card>
+    <v-card-title>
+      <h1>ユーザ: {{ userId }}</h1>
+    </v-card-title>
+    <v-card-actions>
+      <v-btn color="primary" to="friend" append>
+        <v-icon left>mdi-account-group</v-icon>友達一覧
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
